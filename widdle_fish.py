13f20f63706266422e3592ecc2023f06485da000
cwkreid19141 <student+19141@codingwithkids.com>
2019-08-19 16:01:27 -0700

    nyhj

--- widdle_fish.py
+++ widdle_fish.py
@@ -1,3 +1,15 @@
+import pygame
+import sys
+
+pygame.init()
+screen_info = pygame.display.Info()
+
+size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+color = (0, 127, 255)
+
+
 def main():
     print("Hello world")
 
