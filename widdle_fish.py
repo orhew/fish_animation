c3ffb1d080f9f25f9225f4d1671fe5f3f8a4370c
cwkreid19141 <student+19141@codingwithkids.com>
2019-08-20 15:56:17 -0700

    added new function to add multiple fish of different sizes & speeds

--- widdle_fish.py
+++ widdle_fish.py
@@ -1,7 +1,8 @@
-import pygame
-import random
 import sys
+
 from pygame.locals import *
+
+from fish import *
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -12,38 +13,26 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 color = (0, 127, 255)
-fish_image = pygame.image.load("fish.png")
-fish_image = pygame.transform.smoothscale(fish_image, (80, 80))
-fish_rect = fish_image.get_rect()
-fish_rect.center = (width // 2, height // 2)
-speed = pygame.math.Vector2(0, 10)
-rotation = random.randint(0, 360)
-speed.rotate_ip(rotation)
-fish_image = pygame.transform.rotate(fish_image, 180 - rotation)
-
-
-def move_fish():
-    global fish_image
-    fish_rect.move_ip(speed)
-    if fish_rect.left < 0 or fish_rect.right > screen_info.current_w:
-        speed[0] *= -1
-        fish_image = pygame.transform.flip(fish_image, True, False)
-        fish_rect.move_ip(speed[0], 0)
-    if fish_rect.top < 0 or fish_rect.bottom > screen_info.current_h:
-        speed[1] *= -1
-        fish_image = pygame.transform.flip(fish_image, False, True)
-        fish_rect.move_ip(0, speed[1])
+fishes = []
 
 
 def main():
+    for i in range(10):
+        fishes.append(Fish((width/2, height/2)))
     while True:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
-        move_fish()
+            if event.type == KEYDOWN:
+                if event.key == K_d:
+                    for i in range(len(fishes) // 2):
+                        fishes.pop(0)
         screen.fill(color)
-        screen.blit(fish_image, fish_rect)
+        for fish in fishes:
+            fish.update()
+        for fish in fishes:
+            fish.draw(screen)
         pygame.display.flip()
 
 
